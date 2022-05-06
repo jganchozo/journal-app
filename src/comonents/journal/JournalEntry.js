cde48277f0a35c44a1a83582c5aac42301ea480b
jganchozo <jganchozo@outlook.com>
2022-05-06 05:51:46 -0500

    dates

--- src/comonents/journal/JournalEntry.js
+++ src/comonents/journal/JournalEntry.js
@@ -1,29 +1,36 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+
+    const noteDate = moment(date);
+    console.log(noteDate);
+    
     return (
         <div className='journal__entry pointer'>
-            <div
-                className='journal__entry-picture'
-                style={{
-                    backgroundSize: 'cover',
-                    backgroundImage: 'url(https://www.capturelandscapes.com/wp-content/uploads/2019/04/Desert-Nights.jpg)',
-                }}
-            >
-            </div>
+            {
+                url &&
+                <div
+                    className='journal__entry-picture'
+                    style={{
+                        backgroundSize: 'cover',
+                        backgroundImage: `url(${url})`,
+                    }}
+                ></div>
+            }
 
             <div className='journal__entry-body'>
                 <p className='journal__entry-title'>
-                    Un nuevo dia
+                    { title }
                 </p>
                 <p className='journal__entry-content'>
-                    Reprehenderit id in duis consectetur deserunt veniam fugiat.
+                    { body }
                 </p>
             </div>
 
             <div className='journal__entry-date-box'>
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{noteDate.format('dddd')}</span>
+                <h4>{noteDate.format('Do')}</h4>
             </div>
         </div>
     )
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,6 +7,8 @@ import  { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
 import { PrivateRoutes } from './PrivateRoutes';
 import { PublicRoutes } from './PublicRoutes';
+import { loadNotes } from '../helpers/loadNotes';
+import { setNotes, startLoadingNotes } from '../actions/notes';
 
 export const AppRouter = () => {
 
@@ -15,10 +17,12 @@ export const AppRouter = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
-        firebase.auth().onAuthStateChanged((user) => {
+        firebase.auth().onAuthStateChanged( async (user) => {
             if (user?.uid) {
                 dispatch(login(user.uid, user.displayName));
                 setIsLoggedIn(true);
+
+                dispatch(startLoadingNotes(user.uid));
             }else{
                 setIsLoggedIn(false);
             }
@@ -30,7 +34,7 @@ export const AppRouter = () => {
 
     if (checking) {
         return (
-            <h1>Espere...</h1>
+            <h1>Please Wait...</h1>
         )
     }
 
